6f2b1be8c129610f3d17187162532ef2be5e8238
Brekk <brekk@brekkbockrath.com>
2020-07-10 15:27:45 +0200

    Update config.fish

--- config.fish
+++ config.fish
@@ -14,8 +14,8 @@ if not functions -q fisher
     fish -c fisher
 end
 
-# starship theme
-eval (starship init fish)
+# yes captain
+starship init fish | source
 
 # snang
 alias lsnang="~/work/snang/snang.js"
@@ -32,106 +32,10 @@ alias bashgat="bat ~/.config/fish/config.fish | rg"
 
 # the best editor on the planet
 
+set -gx EDITOR vim
 alias vivify="vi ~/work/dotfiles/.vimrc"
 
 # the basics
-
-alias ..="cd .."
-alias ll="ls -Agplash"
-
-# git
-
-alias gs="git status"
-alias branch="git branch | grep '*' | lsnang -P 'split(C._) | last'"
-alias gitpurty="git log --oneline --decorate --graph"
-alias flush="git push --set-upstream origin $branch --force-with-lease"
-
-function figfont
-  figlet -l | lsnang -P "split(C.n) | filter(I) | z => z[Math.floor(Math.random() * z.length)]"
-end
-
-function figtext
-  figfont | lsnang -P "z => 'figlet -f \'' + z + '\' \'$argv\'' | exec" | lolcat
-end
-
-function figtest
-  figlet -l | lsnang -P "split(C.n) | filter(I) | tackOn(z => Math.round((z.length - 1) * Math.random())) | ([l, i]) => l[i] | trace('picked:') | z => 'figlet -f \'' + z + '\' \'$argv\'' | exec" | lolcat
-end
-
-# z is autojump
-alias j='z'
-
-# bat cat with wings
-alias cat="bat --tabs=2"
-
-# ping ping ping
-alias ping="prettyping --nolegend"
-
-# yarn! offline!
-alias jarn='yarn --prefer-offline'
-
-function fish_greeting
-  figtext (pwd | lsnang -P "split(C.s) | map(trim) | last")
-end
-
-# let them know you're repping flipmode
-set -gx EDITOR vim
-
-alias sed="gsed"
-
-# alias cargo="$HOME/.cargo/bin/cargo"
-# alias rustup="$HOME/.cargo/bin/rustup"
-alias knock="~/work/binoculars/binocular.js"
-
-alias gpsup="git push -u origin (branch)"
-function nname
-  available $1 -r
-end
-
-
-# sssshhhhh, it's secure
-# eval (ssh-agent -c)
-
-# if test -z (pgrep ssh-agent)
-#   eval "(ssh-agent -c)" >/dev/null
-#   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-#   set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-#   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-# end
-fish_ssh_agent
-
-# byobu / tmux
-# set BYOBU_PREFIX /usr/local
-
-# yes captain
-# starship init fish | source
-
-# the fuck
-# thefuck --alias | source
-
-# meta utilities
-if available tmux
-  alias whodat="tmux ls"
-  alias sup="tmux attach -t"
-  alias peace="tmux detach"
-end
-if available bat
-  alias cat="bat --tabs=2"
-end
-if available exa
-  alias ls=exa
-  alias ll='exa --all --long --header --grid --ignore-glob="*.un~|*node_modules|*.git*" --git-ignore'
-else
-  alias ll="ls -Agplash"
-end
-
-alias fishpro="vi ~/.config/fish/config.fish"
-alias fishsource="source ~/.config/fish/config.fish"
-alias fishbat="cat ~/.config/fish/config.fish"
-alias fishgat="cat ~/.config/fish/config.fish | rg"
-alias vivify="vi ~/work/dotfiles/.vimrc"
-
-# old habits die hard
 
 alias ..="cd .."
 
@@ -139,20 +43,32 @@ alias bashpro='fishpro'
 alias bashsource='fishsource'
 alias j="z" # never washed
 
-# ripgrep
-set -x RIPGREP_CONFIG_PATH /Users/bbockrath/.ripgreprc
-
-# inimitable
+# git
 
 alias gs="git status"
-alias branch="git branch | grep '*' | snang -P 'split(C._) | last'"
+alias branch="git branch | grep '*' | lsnang -P 'split(C._) | last'"
 alias gitpurty="git log --oneline --decorate --graph"
+alias flush="git push --set-upstream origin $branch --force-with-lease"
 alias gpsup="git push -u origin (branch)"
 
-# other languages
+# ping ping ping
+alias ping="prettyping --nolegend"
 
-## javasc-- oh, gross
-set JAVA_HOME /Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
+alias sed="gsed"
+
+# alias cargo="$HOME/.cargo/bin/cargo"
+# alias rustup="$HOME/.cargo/bin/rustup"
+
+function nname
+  available $1 -r
+end
+
+fish_ssh_agent
+
+# ripgrep
+set -x RIPGREP_CONFIG_PATH ~/.ripgreprc
+
+# other languages
 
 ## why would I ever want an infinite list?
 set GHCUP_INSTALL_BASE_PREFIX = $HOME
@@ -161,7 +77,6 @@ set PATH $HOME/.ghcup/bin $PATH
 
 ## make sure you have tetanus
 source $HOME/.cargo/env
-
 
 # fun
 
@@ -179,11 +94,51 @@ function figtest
   figlet -f "$TEST_FONT" $argv[1] | lolcat
 end
 
-function fish_greeting
-  figtest (basename (pwd))
+# tmux
+alias whodat="tmux ls"
+alias yo="tmux new -s"
+alias sup="tmux attach -t"
+alias peace="tmux detach"
+
+# standard tools
+alias cat="bat --tabs=2"
+alias ls=exa
+alias ll='exa --all --long --header --grid --ignore-glob="*node_modules|*.git*|*.un~|*.swp" --git-ignore'
+
+# ripgrep
+set -x RIPGREP_CONFIG_PATH /Users/bbockrath/.ripgreprc
+
+# other languages
+
+## why would I ever want an infinite list?
+set GHCUP_INSTALL_BASE_PREFIX = $HOME
+set PATH $HOME/.cabal/bin $PATH
+set PATH $HOME/.ghcup/bin $PATH
+
+## make sure you have tetanus
+source $HOME/.cargo/env
+
+function testDeprecated
+  npm i 2>&1 | rg 'deprecated' | tee deprecated.log
+  figtext 'deprecated' | lolcat
 end
 
-function fuckf5
-  sudo cp /etc/hosts ~/hosts.bak
-  sudo cp /etc/hosts-before-stupid-f5 /etc/hosts
+function allRequires
+  rg "require\(" | snang -P "split(C.n) | filter(I) | map(pipe(split(':'), slice(1, Infinity), nth(0), y => y.substr(y.indexOf('require') + 9, y.indexOf(')')) | replace(';', '') | replace(',', '') | z => { var a = z.indexOf(').'); return a > -1 ? z.slice(0, a) : z } | replace('\"', '') | replace(')', '') | z => z.indexOf('.') === 0 ? '~' + z.slice(z.lastIndexOf('/') + 1, Infinity) : z)) | uniq | filter(I) | sort((a, b) => a === b ? 0 : a > b ? 1 : -1) | j2"
+end
+
+alias xovi="xargs -o vi"
+
+function griprep
+  rg -l $argv[1] | uniq | xargs echo | xovi
+end
+
+alias stripColors="gsed -r \"s/\x1B\[([0-9]{1,3}(;[0-9]{1,2})?)?[mGK]//g\""
+
+function crumb --description 'leaves a trail of crumbs'
+  tee last-run.log && cat last-run.log
+end
+
+function brb --description 'cd into a directory, execute a command, then cd back.'
+  j $argv[1] && eval $argv[2..-1] && cd -
 end
