cdf80a245fde930f698022df960db0ae6290177f
Brekk <brekk@brekkbockrath.com>
2021-02-21 18:39:46 +0100

    update for haskell and more fisher stuff

--- fisher-plugins.fish
+++ fisher-plugins.fish
@@ -5,6 +5,7 @@ franciscolourenco/done \
 igalic/anicode \
 jethrokuan/fzf \
 jethrokuan/z \
+jorgebucaran/nvm.fish \
 jorgebucaran/fish-bax \
 jorgebucaran/fish-getopts \
 jorgebucaran/fish-nvm \
---
+++ haskell.fish
@@ -0,0 +1,2 @@
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+curl -sSL https://get.haskellstack.org/ | sh
